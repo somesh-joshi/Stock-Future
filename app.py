283493aed40622cdaf3a8a3682916acc00558d3a
Somesh Joshi <someshjoshi890@gmail.com>
2022-04-07 16:02:37 +0530

    last most update.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import libraries
 from audioop import reverse
 from doctest import master
+from statistics import mean
+from tkinter import Menu
 from keras import models
 import numpy as np
 import pandas as pd
@@ -8,6 +10,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader as data
 import streamlit as st
 from datetime import date, timedelta 
+import yfinance as yf
 
 from sklearn.metrics import accuracy_score
 
@@ -19,7 +22,7 @@ end = today
 st.title('Stock Price Prediction')
 
 # get stock symbol from user
-user_input = st.text_input("Enter The Stock Symbol", 'TATAPOWER.NS')
+user_input = st.text_input("Enter The Stock Symbol", 'TATAMOTORS.NS')
 
 end = st.text_input("Enter The Date", end)
 # convart end date to datetime
@@ -81,30 +84,54 @@ scaler_factor = 1/scaler[0]
 predicted = (y_predicted * scaler_factor)
 flat_predicted = predicted.flatten()
 ma_predicted = np.sort(flat_predicted)
-e1 = ma_predicted.max()
-e2 = data_testing.tail(1).Close[0]
 
 st.subheader('Predicted Price Graph')
 fig = plt.figure(figsize=(12,6))
 plt.plot(ma_predicted)
 st.pyplot(fig)
 
+st.header('Predicted Results')
 # Table
-table_data_original = data_testing.Close[-6:]
-table_data_predicted = flat_predicted[-6:]
+table_data_original = data_testing.Close[-5:]
+table_data_predicted = flat_predicted[-5:]
 table_data_original = table_data_original[:5]
 table_data_predicted = table_data_predicted[:5]
 master_table = pd.DataFrame({'Original Price': table_data_original, 'Predicted Price': table_data_predicted, 'Error Percentage': abs((table_data_predicted-table_data_original)/table_data_original)*100})
 st.table(master_table) 
+e1 = table_data_predicted[4]
+e2 = table_data_original[4]
+# mean 
+e3 = mean(table_data_predicted)
+e4 = mean(table_data_original)
 
-
+st.header('Daily')
 # get prediction
-st.subheader('Prediction: ' + str(e1))
+st.subheader('daily Prediction: ' + str(e1))
 
 # get original price
 st.subheader('Original Price: ' + str(e2))
 
 # get error percentage
 st.subheader('Error Percentage: ' + str(abs((e1-e2)/e2)*100))
+
+st.markdown("""---""")
+
+st.header('Weekly')
+
+#date one week ago
+date_one_week_ago = end - timedelta(days=6)
+st.write('From ' + str(date_one_week_ago) + ' to ' + str(end))
+
+# get prediction
+st.subheader('weekly Prediction: ' + str(e3))
+
+# get original price
+st.subheader('Original Price: ' + str(e4))
+ 
+# get error percentage
+
+st.subheader('Error Percentage: ' + str(abs((e3-e4)/e4)*100))
+
+
 
    
